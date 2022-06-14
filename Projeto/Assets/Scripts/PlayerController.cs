0a012b64a4ece54d063e426c76f5a339a9ff1dd8
Jamilly Cristine Nascimento de Freitas <jamilly.f@academico.ifrn.edu.br>
2022-06-14 19:13:26 -0300

    labirinto

--- Projeto/Assets/Scripts/PlayerController.cs
+++ Projeto/Assets/Scripts/PlayerController.cs
@@ -12,28 +12,26 @@ public class PlayerController : MonoBehaviour
     
     private GameControl _gameControl;
     private PlayerInput _playerInput;
-    private Camera mainCamera;
-    private Rigidbody rigidbody;
+    private Camera _mainCamera;
+    private Rigidbody _rigidbody;
 
-    private Vector2 _moveInput;
+    private Vector2 _moverInput;
 
     private void OnEnable()
     {
         //inicializacao de variavel 
-
         _gameControl = new GameControl();
         
         //referencias dos componentes no mesmo projeto da unity
-
         _playerInput = GetComponent<PlayerInput>();
-        rigidbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
         
         //referencias para a camera main guardar na classe camera
-        mainCamera = Camera.main;
+        _mainCamera = Camera.main;
         
-        //atribuindo o delegate do action triggerd no player input 
-
-        _playerInput.onActionTriggered -= OnActionTriggered;
+        
+        //Articuando ao delegate do action triggerd do player input
+        _playerInput.onActionTriggered += OnActionTiggered;
         
 
     }
@@ -45,37 +43,37 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnActionTiggered(InputAction.CallbackContext obj)
-    
-        //comecando o nome do action que esta chegando com o nome do action moviment 
-
     {
-        if (obj.action.name.CompareTo(_gameControl.Gameplay.Moviment.name)== 0)
-            
-
+        //comecando o nome do action que esta chegando com o  nome do action no moviment 
+        if (obj.action.name.CompareTo(_gameControl.Gameplay.Moviment.name) == 0) 
         {
+            // atribuir ao moveinput do jogador com um vector2
+            _moverInput= obj.ReadValue <Vector2> ();
             
-            //atribuir ao moveinput o valor proveniente ao input do jogador com um vector2
-
-            _moveInput = obj.ReadValue<Vector2>();
-            
-            
-
         }
+    }
 
+    private void move()
 
     {
-        //calcule  do movimento no eixo da camera para movimento frente/tras
-        Vector3 moveVertical = mainCamera.transform.forward * _moveInput.y;
-        //calcule o movimento no eixo da camera para o movimento esquerda/direita
-        Vector3 MoverHorizontal =
-            
-       
-            
+        //calcula o movimento do eixo da camera para movimento frente/tras
+        Vector3 moveVertical = _mainCamera.transform.forward * _moverInput.y;
+        
+        
+        //calcula o movimento no eixo da camera para movimento esquerdo/direita
+        Vector3 moveHorizontal = _mainCamera.transform.right* _moverInput.x;
+        
+        //adiciona a forca no objeto pelo rigidbody
+        _rigidbody.AddForce ((moveVertical + moveHorizontal ) *moveSpeed*Time.fixedDeltaTime);
 
         
     }
-   
-    
-}
+
+    private void FixedUpdate()
+    {
+        move ();
+
+    }
+    }
 
 
